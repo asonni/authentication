b37842bca26cf3631106b1e52aa1002ea3f62c39
Aladdin Sonni <alaaden3000@gmail.com>
2017-08-25 01:29:07 +0200

    add pm2 configration file and and doing some changes

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,27 +4,25 @@ import { AUTH_USER, AUTH_ERROR, UNAUTH_USER, FETCH_MESSAGE } from './Types';
 
 const ROOT_URL = 'http://localhost:3090';
 
-export const signinUser = ({ email, password }, callback) => {
-  return dispatch => {
-    // Submit email/password to the server
-    axios
-      .post(`${ROOT_URL}/signin`, { email, password })
-      .then(response => {
-        // If request is good...
-        // - Update state to indicate user is authenticated
-        dispatch({ type: AUTH_USER });
-        // - Save the JWT token
-        localStorage.setItem('token', response.data.token);
-        // - redirect to the route '/feature'
-        callback();
-      })
-      .catch(() => {
-        // If request is bad...
-        // - Show an error to the user
-        dispatch(authError('Bad Login Info'));
-        dispatch(reset('signin'));
-      });
-  };
+export const signinUser = ({ email, password }, callback) => dispatch => {
+  // Submit email/password to the server
+  axios
+    .post(`${ROOT_URL}/signin`, { email, password })
+    .then(response => {
+      // If request is good...
+      // - Update state to indicate user is authenticated
+      dispatch({ type: AUTH_USER });
+      // - Save the JWT token
+      localStorage.setItem('token', response.data.token);
+      // - redirect to the route '/feature'
+      callback();
+    })
+    .catch(() => {
+      // If request is bad...
+      // - Show an error to the user
+      dispatch(authError('Bad Login Info'));
+      dispatch(reset('signin'));
+    });
 };
 
 export const signupUser = ({ email, password }, callback) => {
