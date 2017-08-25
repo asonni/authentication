46c04f8ecc481db31e47a65e7ed3202bdd415442
Aladdin Sonni <alaaden3000@gmail.com>
2017-08-25 09:53:23 +0200

    change async await instead of usual .then promise

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,42 +4,42 @@ import { AUTH_USER, AUTH_ERROR, UNAUTH_USER, FETCH_MESSAGE } from './Types';
 
 const ROOT_URL = 'http://localhost:3090';
 
-export const signinUser = ({ email, password }, callback) => dispatch => {
-  // Submit email/password to the server
-  axios
-    .post(`${ROOT_URL}/signin`, { email, password })
-    .then(response => {
-      // If request is good...
-      // - Update state to indicate user is authenticated
-      dispatch({ type: AUTH_USER });
-      // - Save the JWT token
-      localStorage.setItem('token', response.data.token);
-      // - redirect to the route '/feature'
-      callback();
-    })
-    .catch(() => {
-      // If request is bad...
-      // - Show an error to the user
-      dispatch(authError('Bad Login Info'));
-      dispatch(reset('signin'));
+export const signinUser = ({ email, password }, callback) => async dispatch => {
+  try {
+    // Submit email/password to the server
+    const response = await axios.post(`${ROOT_URL}/signin`, {
+      email,
+      password
     });
+    // If request is good...
+    // - Update state to indicate user is authenticated
+    dispatch({ type: AUTH_USER });
+    // - Save the JWT token
+    localStorage.setItem('token', response.data.token);
+    // - redirect to the route '/feature'
+    callback();
+  } catch (error) {
+    // If request is bad...
+    // - Show an error to the user
+    dispatch(authError('Bad Login Info'));
+    dispatch(reset('signin'));
+  }
 };
 
-export const signupUser = ({ email, password }, callback) => {
-  return dispatch => {
-    axios
-      .post(`${ROOT_URL}/signup`, { email, password })
-      .then(response => {
-        dispatch({ type: AUTH_USER });
-        localStorage.setItem('token', response.data.token);
-        callback(true);
-      })
-      .catch(({ response }) => {
-        dispatch(authError(response.data.error));
-        dispatch(reset('signup'));
-        callback(false);
-      });
-  };
+export const signupUser = ({ email, password }, callback) => async dispatch => {
+  try {
+    const response = await axios.post(`${ROOT_URL}/signup`, {
+      email,
+      password
+    });
+    dispatch({ type: AUTH_USER });
+    localStorage.setItem('token', response.data.token);
+    callback(true);
+  } catch ({ response }) {
+    dispatch(authError(response.data.error));
+    dispatch(reset('signup'));
+    callback(false);
+  }
 };
 
 export const authError = error => {
@@ -54,26 +54,27 @@ export const signoutUser = () => {
   return { type: UNAUTH_USER };
 };
 
-// export const fetchMessage = () => {
-//   return dispatch => {
-//     axios.get(ROOT_URL, {
-//       headers: { authorization: localStorage.getItem('token') }
-//     }).then(response => {
-//       dispatch({
-//         type: FETCH_MESSAGE,
-//         payload: response.data.message
-//       });
-//     });
-//   }
-// }
-
-// This code how can we using redux promise instead of redux thunk
-export const fetchMessage = () => {
-  const request = axios.get(`${ROOT_URL}/secret`, {
-    headers: { authorization: localStorage.getItem('token') }
-  });
-  return {
-    type: FETCH_MESSAGE,
-    payload: request
-  };
+export const fetchMessage = () => async dispatch => {
+  try {
+    const response = await axios.get(`${ROOT_URL}/secret`, {
+      headers: { authorization: localStorage.getItem('token') }
+    });
+    dispatch({
+      type: FETCH_MESSAGE,
+      payload: response
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+// This code how can we use redux promise instead of redux thunk
+// export const fetchMessage = () => {
+//   const request = axios.get(`${ROOT_URL}/secret`, {
+//     headers: { authorization: localStorage.getItem('token') }
+//   });
+//   return {
+//     type: FETCH_MESSAGE,
+//     payload: request
+//   };
+// };
